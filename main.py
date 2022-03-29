3a1789dc31d10ecc493814303da997ea7606c542
theCrackbot <101562650+theCrackbot@users.noreply.github.com>
2022-03-30 01:05:13 +0530

    Refactored code using try and catch method using keywords

--- main.py
+++ main.py
@@ -43,16 +43,21 @@ def save():
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="OOPS!!", message="Please make sure to fill all the fields")
     else:
-        with open("data.json", 'r') as data_file:
-            # Reading old data
-            data = json.load(data_file)
+        try:
+            with open("data.json", 'r') as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", 'w') as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
             # Updating old data with new data
             data.update(new_data)
 
-        with open("data.json", 'w') as data_file:
-            # Saving updated data
-            json.dump(data, data_file, indent=4)
-
+            with open("data.json", 'w') as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
